042026b944d4ff9ab67825941dcfdc01ab40735e
dandov <dandovdel@gmail.com>
2020-01-08 22:10:05 -0800

    image works now, but it gets weird when I do 960*540

--- raytracing/main.cc
+++ raytracing/main.cc
@@ -1,9 +1,5 @@
-#include <fstream>
 #include <iostream>
-#include <sstream>
 #include <vector>
-#include <stdio.h>
-#include <stdlib.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -12,45 +8,28 @@
 
 int main(int argc, char** argv) {
 	std::cout << "HENLO!!\n";
-	const size_t width = 960;
-	const size_t height = 540;
+	const size_t width = 100;
+	const size_t height = 100;
 	const size_t channels = 3;
 
-	std::vector<int> image_bytes(width * height * channels);
+	std::vector<uint8_t> image_bytes(width * height * channels);
 	for (size_t i = 0; i < height; ++i) {
 		for (size_t j = 0; j < width; ++j) {
 			const size_t coord = i * width * channels + j * channels;
-			const float blue_ratio = static_cast<float>(j) / static_cast<float>(height);
-			image_bytes[coord] = 0;  // red
+			const float ratio = static_cast<float>(j) / static_cast<float>(height);
+			image_bytes[coord] = static_cast<uint8_t>((1.f - ratio) * 255.f);  // red
 			image_bytes[coord + 1] = 0;  // green
-			image_bytes[coord + 2] = static_cast<int>(255.f * blue_ratio);  // blue
+			image_bytes[coord + 2] = static_cast<uint8_t>(ratio * 255.f);  // blue
 		}
 	}
 
-	std::stringstream ss;
-	ss << "P3\n";
-	ss << width << " " << height << "\n";
-	ss << "255\n";
-	for (size_t i = 0; i < height; ++i) {
-		for (size_t j = 0; j < width; ++j) {
-			const size_t coord = i * width * channels + j * channels;
-			ss << image_bytes[coord] << " "
-			   << image_bytes[coord + 1] << " "
-			   << image_bytes[coord + 2] << "\n";
-		}
-	}
-
-	std::ofstream image_file;
-	image_file.open("image.ppm");
-	image_file << ss.str();
-	image_file.close();
-
-	constexpr int STBI_RGB_FORMAT = 3; // 1=Y, 2=YA, 3=RGB, 4=RGBA
+	constexpr int STBI_RGB_FORMAT = channels; // 1=Y, 2=YA, 3=RGB, 4=RGBA
 	// Distance in bytes from the first byte of a row of pixels to
 	// the first byte of the next row of pixels.
 	const int stride = width * channels;
 	const int result = stbi_write_png("image.png", width, height, 
 		STBI_RGB_FORMAT, &image_bytes[0], stride);
+	stbi_write_jpg("image.jpg", width, height, channels, &image_bytes[0], 100);
 	if (!result) {
 		std::cout << "Failed to write image...";
 		return 1;
