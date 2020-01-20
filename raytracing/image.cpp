2061d1b90190bceaa8f436855f0d9b3b060d5c20
dandov <dandovdel@gmail.com>
2020-01-19 23:54:18 -0800

    Moved everything to a class Image, add image update to render loop

---
+++ raytracing/image.cpp
@@ -0,0 +1,83 @@
+#include "image.h"
+
+#include <iostream>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+namespace RayTracing {
+
+ImageProperties::ImageProperties()
+	: width(0), height(0), channels(0) {}
+
+ImageProperties::ImageProperties(
+		int i_width, int i_height, int i_channels)
+	: width(i_width),
+	  height(i_height),
+	  channels(i_channels) {}
+
+bool Image::Init(const ImageProperties& properties) {
+	if (pixels_) {
+		return false;
+	}
+
+	
+	// Create empty texture.
+	if (!texture_.create(properties.width, properties.height)) {
+		std::cout << "Error creating texture.";
+		return false;
+	}
+
+	properties_ = properties;
+	
+	pixels_ = std::make_unique<sf::Uint8[]>(
+		properties_.width * properties_.height * properties_.channels);
+	texture_.update(pixels_.get());
+	sprite_.setTexture(texture_);
+}
+
+void Image::Update(double dt_ms) {
+	for (size_t i = 0; i < properties_.height; ++i) {
+		for (size_t j = 0; j < properties_.width; ++j) {
+			const size_t coord =
+				i * properties_.width * properties_.channels + j * properties_.channels;
+			const float horizontal_ratio =
+				static_cast<float>(j) / static_cast<float>(properties_.width);
+			const float vertical_ratio =
+				static_cast<float>(i) / static_cast<float>(properties_.height);
+			pixels_[coord] = static_cast<uint8_t>((horizontal_ratio) * 255.f);  // red
+			pixels_[coord + 1] = static_cast<uint8_t>((1.f - vertical_ratio) * 255.f);  // green
+			pixels_[coord + 2] = 0u;  // blue
+			pixels_[coord + 3] = 255u;  // alpha
+		}
+	}
+	texture_.update(pixels_.get());
+}
+
+void Image::Render(sf::RenderWindow* window) {
+	window->draw(sprite_);
+}
+
+bool Image::WriteToFile(const std::string& file_name, Image::FileFormat file_format) {
+	// Distance in bytes from the first byte of a row of pixels to
+	// the first byte of the next row of pixels.
+	const int stride = properties_.width * properties_.channels;
+	int result = 0;
+	switch (file_format) {
+		case Image::FileFormat::JPEG:
+			result = stbi_write_png(file_name.c_str(),
+				properties_.width, properties_.height,
+				properties_.channels, pixels_.get(), stride);
+			break;
+		case Image::FileFormat::PNG:
+			stbi_write_jpg(file_name.c_str(), properties_.width, properties_.height,
+				properties_.channels, pixels_.get(), 100);
+			break;
+	};
+
+	return result;
+}
+
+}  // namespace RayTracing
--- raytracing/main.cc
+++ raytracing/main.cc
@@ -1,11 +1,6 @@
 #include <iostream>
-#include <vector>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-
+#include "image.h"
 #include "SFML/Graphics.hpp"
 
 int main(int argc, char** argv) {
@@ -16,28 +11,8 @@ int main(int argc, char** argv) {
 	
 	// Create an SFML window.
 	sf::RenderWindow window(sf::VideoMode(width, height), "Ray Tracing in One Weekend!!!");
-	sf::Texture texture;
-	// Create empty texture.
-	if (!texture.create(width, height)) {
-		std::cout << "Error creating texture.";
-	}
-
-	sf::Uint8* pixels = new sf::Uint8[width * height * channels];
-	// pixels.resize(width * height * channels);
-	for (size_t i = 0; i < height; ++i) {
-		for (size_t j = 0; j < width; ++j) {
-			const size_t coord = i * width * channels + j * channels;
-			const float horizontal_ratio = static_cast<float>(j) / static_cast<float>(width);
-			const float vertical_ratio = static_cast<float>(i) / static_cast<float>(height);
-			pixels[coord] = static_cast<uint8_t>((horizontal_ratio) * 255.f);  // red
-			pixels[coord + 1] = static_cast<uint8_t>((1.f - vertical_ratio) * 255.f);  // green
-			pixels[coord + 2] = 0u;  // blue
-			pixels[coord + 3] = 255u;  // alpha
-		}
-	}
-	texture.update(&pixels[0]);
-	sf::Sprite sprite;
-	sprite.setTexture(texture);
+	RayTracing::Image image;
+	image.Init(RayTracing::ImageProperties(width, height, channels));
 
 	// run the program as long as the window is open
 	while (window.isOpen()) {
@@ -53,24 +28,17 @@ int main(int argc, char** argv) {
 		// Clear the window with black color.
 		window.clear(sf::Color::Black);
 
-		// Draw image.
-		window.draw(sprite);
+		image.Update(0.0);
+		image.Render(&window);
 
 		// End the current frame.
 		window.display();
 	}
 
-	// Distance in bytes from the first byte of a row of pixels to
-		// the first byte of the next row of pixels.
-	const int stride = width * channels;
-	// TODO(dandov): This is lighter after changing to RGBA.
-	const int result = stbi_write_png("image.png", width, height,
-		channels, pixels, stride);
-	stbi_write_jpg("image.jpg", width, height, channels, pixels, 100);
-	if (!result) {
-		std::cout << "Failed to write image...";
-		return 1;
-	}
+	const bool write_file_result =
+		image.WriteToFile("image.png", RayTracing::Image::FileFormat::PNG);
+	if (!write_file_result)
+		std::cout << "Failed to write image.";
 
 	std::cout << "Finished...\n";
 	return 0;
