e5ef9ea24436a8c09efb7b78846775c30a7cca2c
dandov <dandovdel@gmail.com>
2020-01-20 12:42:01 -0800

    Minor nits and style adjustments...

--- raytracing/main.cc
+++ raytracing/main.cc
@@ -4,19 +4,21 @@
 #include "SFML/Graphics.hpp"
 
 int main(int argc, char** argv) {
-	std::cout << "HENLO!!\n";
+	std::cout << "Ray Tracing!!\n";
 	const size_t width = 960;
 	const size_t height = 540;
 	const size_t channels = 4;
 	
 	// Create an SFML window.
-	sf::RenderWindow window(sf::VideoMode(width, height), "Ray Tracing in One Weekend!!!");
+	sf::RenderWindow window(
+		sf::VideoMode(width, height), "Ray Tracing in One Weekend!!!");
 	RayTracing::Image image;
 	image.Init(RayTracing::ImageProperties(width, height, channels));
 
 	// run the program as long as the window is open
 	while (window.isOpen()) {
-		// check all the window's events that were triggered since the last iteration of the loop
+		// Check all the window's events that were triggered since the last
+		// iteration of the loop.
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			// "close requested" event: we close the window
@@ -35,9 +37,7 @@ int main(int argc, char** argv) {
 		window.display();
 	}
 
-	const bool write_file_result =
-		image.WriteToFile("image.png", RayTracing::Image::FileFormat::PNG);
-	if (!write_file_result)
+	if (!image.WriteToFile("image.png", RayTracing::Image::FileFormat::PNG))
 		std::cout << "Failed to write image.";
 
 	std::cout << "Finished...\n";
