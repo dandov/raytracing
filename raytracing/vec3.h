0621741977472a549e05c89d9b643f7d78a15fc4
Daniel Dovali <danieldovali@Daniels-MacBook-Pro.local>
2020-01-22 22:56:58 -0800

    Continue implementing vec3 class

--- raytracing/vec3.h
+++ raytracing/vec3.h
@@ -1,6 +1,8 @@
 #ifndef RAYTRACING_VEC3
 #define RAYTRACING_VEC3
 
+#include <cmath>
+
 class vec3 {
 	public:
 		vec3() = default;
@@ -10,9 +12,9 @@ class vec3 {
 			data[2] = e2;
 		}
 
+        inline float x() const { return e[0]; }
 		inline float y() const { return e[1]; }
 		inline float z() const { return e[2]; }
-		inline float x() const { return e[0]; }
 		inline float r() const { return e[0]; }
 		inline float g() const { return e[1]; }
 		inline float b() const { return e[2]; }
@@ -21,8 +23,58 @@ class vec3 {
 		inline vec3 operator-() { return vec3(-e[0], -e[1], -e[2]); }
 		inline float operator[](int i) { return e[í]; }
 		inline float& operator[](int i) { return e[i]; }
-	private:
+    
+        inline vec3& operator+=(const vec3& v2);
+        inline vec3& operator-=(const vec3& v2);
+        inline vec3& operator*=(const vec3& v2);
+        inline vec3& operator/=(const vec3& v2);
+        inline vec3& operator*=(float s);
+        inline vec3& operator/=(float s);
+    
+        inline float SquaredLength() const { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; }
+        inline float length() const { return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]) ; }
+        inline void MakeUnitVector();
+
 		float data[3];
 };
+
+inline vec3 operator+(const vec3& v1, const vec3& v2) {
+    return vec3(v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]);
+}
+
+inline vec3 operator-(const vec3& v1, const vec3& v2) {
+    return vec3(v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]);
+}
+
+inline vec3& vec3::operator+=(const vec3& v2) {
+    e[0] += v2.x();
+    e[1] += v2.y();
+    e[2] += v2.z();
+    return *this;
+}
+
+inline vec3& vec3::operator-=(const vec3& v2) {
+    e[0] -= v2.x();
+    e[1] -= v2.y();
+    e[2] -= v2.z();
+    return *this;
+}
+
+inline vec3& vec3::operator*=(const vec3& v2) {
+    e[0] *= v2.x();
+    e[1] *= v2.y();
+    e[2] *= v2.z();
+    return *this;
+}
+
+inline vec3& vec3::operator/=(const vec3& v2) {
+    e[0] /= v2.x();
+    e[1] /= v2.y();
+    e[2] /= v2.z();
+    return *this;
+}
+
+inline vec3& vec3::operator*=(float s);
+inline vec3& vec3::operator/=(float s);
 
 #endif RAYTRACING_VEC3
