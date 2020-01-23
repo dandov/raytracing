2a0e0c317267c695a75f2daac772aa0bcc2a8928
dandov <dandovdel@gmail.com>
2020-01-22 23:23:32 -0800

    Added more operator overrides for vec3

--- raytracing/vec3.h
+++ raytracing/vec3.h
@@ -1,28 +1,29 @@
 #ifndef RAYTRACING_VEC3
 #define RAYTRACING_VEC3
 
+#include <iostream>
 #include <cmath>
 
 class vec3 {
 	public:
 		vec3() = default;
-		vec3(float e0, float e1, float e3) {
+		vec3(float e0, float e1, float e2) {
 			data[0] = e0;
 			data[1] = e1;
 			data[2] = e2;
 		}
 
-        inline float x() const { return e[0]; }
-		inline float y() const { return e[1]; }
-		inline float z() const { return e[2]; }
-		inline float r() const { return e[0]; }
-		inline float g() const { return e[1]; }
-		inline float b() const { return e[2]; }
+        inline float x() const { return data[0]; }
+		inline float y() const { return data[1]; }
+		inline float b() const { return data[2]; }
+		inline float z() const { return data[2]; }
+		inline float r() const { return data[0]; }
+		inline float g() const { return data[1]; }
 
 		inline const vec3& operator+() { return *this;  }
-		inline vec3 operator-() { return vec3(-e[0], -e[1], -e[2]); }
-		inline float operator[](int i) { return e[í]; }
-		inline float& operator[](int i) { return e[i]; }
+		inline vec3 operator-() { return vec3(-data[0], -data[1], -data[2]); }
+		inline float operator[](int i) const { return data[i]; }
+        // inline float& operator[](int i) const { return data[i]; }
     
         inline vec3& operator+=(const vec3& v2);
         inline vec3& operator-=(const vec3& v2);
@@ -31,9 +32,9 @@ class vec3 {
         inline vec3& operator*=(float s);
         inline vec3& operator/=(float s);
     
-        inline float SquaredLength() const { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; }
-        inline float length() const { return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]) ; }
-        inline void MakeUnitVector();
+        inline float SquaredLength() const { return data[0] * data[0] + data[1] * data[1] + data[2] * data[2]; }
+        inline float length() const { return sqrt(data[0] * data[0] + data[1] * data[1] + data[2] * data[2]) ; }
+        inline void Normalize();
 
 		float data[3];
 };
@@ -46,35 +47,89 @@ inline vec3 operator-(const vec3& v1, const vec3& v2) {
     return vec3(v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]);
 }
 
+inline vec3 operator*(const vec3& v1, const vec3& v2) {
+    return vec3(v1[0] * v2[0], v1[1] * v2[1], v1[2] * v2[2]);
+}
+
+inline vec3 operator*(const vec3& v1, float s) {
+    return vec3(v1[0] * s, v1[1] * s, v1[2] * s);
+}
+
+inline vec3 operator*(float s, const vec3& v1) {
+    return vec3(v1[0] * s, v1[1] * s, v1[2] * s);
+}
+
+inline vec3 operator/(const vec3& v1, const vec3& v2) {
+    return vec3(v1[0] / v2[0], v1[1] / v2[1], v1[2] / v2[2]);
+}
+
+inline vec3 operator/(const vec3& v1, float s) {
+    return vec3(v1[0] / s, v1[1] / s, v1[2] / s);
+}
+
+inline float dot(const vec3& v1, const vec3& v2) {
+    return v1.x() * v2.x() + v1.y() + v2.y() + v1.z() + v2.z();
+}
+
+inline vec3 cross(const vec3& v1, const vec3& v2) {
+    return vec3(
+        v1.y() * v2.z() - v1.z() * v2.y(),
+        v1.z() * v2.x() - v1.x() * v2.z(),
+        v1.x() * v2.y() - v1.y() * v2.x()
+    );
+}
+
 inline vec3& vec3::operator+=(const vec3& v2) {
-    e[0] += v2.x();
-    e[1] += v2.y();
-    e[2] += v2.z();
+    data[0] += v2.x();
+    data[1] += v2.y();
+    data[2] += v2.z();
     return *this;
 }
 
 inline vec3& vec3::operator-=(const vec3& v2) {
-    e[0] -= v2.x();
-    e[1] -= v2.y();
-    e[2] -= v2.z();
+    data[0] -= v2.x();
+    data[1] -= v2.y();
+    data[2] -= v2.z();
     return *this;
 }
 
 inline vec3& vec3::operator*=(const vec3& v2) {
-    e[0] *= v2.x();
-    e[1] *= v2.y();
-    e[2] *= v2.z();
+    data[0] *= v2.x();
+    data[1] *= v2.y();
+    data[2] *= v2.z();
     return *this;
 }
 
 inline vec3& vec3::operator/=(const vec3& v2) {
-    e[0] /= v2.x();
-    e[1] /= v2.y();
-    e[2] /= v2.z();
+    data[0] /= v2.x();
+    data[1] /= v2.y();
+    data[2] /= v2.z();
     return *this;
 }
 
-inline vec3& vec3::operator*=(float s);
-inline vec3& vec3::operator/=(float s);
+inline vec3& vec3::operator*=(float s) {
+    data[0] *= s;
+    data[1] *= s;
+    data[2] *= s;
+    return *this;
+}
+inline vec3& vec3::operator/=(float s) {
+    data[0] /= s;
+    data[1] /= s;
+    data[2] /= s;
+    return *this;
+}
+
+inline void vec3::Normalize() {
+    const float inv_length = 1.f / length();
+    data[0] *= inv_length;
+    data[1] *= inv_length;
+    data[2] *= inv_length;
+}
+
+inline vec3 Normalize(const vec3& v1) {
+    const float inv_length = 1.f / v1.length();
+    return v1 * inv_length;
+}
 
 #endif RAYTRACING_VEC3
